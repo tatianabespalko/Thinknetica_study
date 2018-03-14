431b33a4e02145814cf165a98fa0c32d626d50a8
TBespalko <tbespalko7@gmail.com>
2018-03-14 21:18:21 +0200

    Add task3 and task4

--- ruby/module_02/quadric_equation.rb
+++ ruby/module_02/quadric_equation.rb
@@ -12,4 +12,26 @@
 # Например,  
  #Math.sqrt(16)
  #  вернет 4, т.е. квадратный корень из 16.
- 
+
+puts "Введите значение a:"
+a = gets.chomp.to_f
+
+puts "Введите значение b:"
+b = gets.chomp.to_f
+
+puts "Введите значение c:"
+c = gets.chomp.to_f
+
+d = b**2 - 4 * a * c
+puts "Дискриминант равен: #{d}"
+
+if d > 0
+  x1 = (-b + Math.sqrt(d)) / (2 * a)
+  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  puts "Первый корень: #{x1}, второй корень #{x2}"
+elsif d.zero?
+  x = -b / (2 * a)
+  puts "Единственный корень: #{x}"
+else
+  puts "Корней нет"
+end
